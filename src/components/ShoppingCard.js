493abeb337c84c0c136f7b70599057f0b25e0404
Nam-SS <146443081+Nam-SS@users.noreply.github.com>
2023-10-06 15:02:59 +0000

    changes added

--- src/components/ShoppingCard.js
+++ src/components/ShoppingCard.js
@@ -5,18 +5,32 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { Grid } from '@mui/material';
+import { Grid,TextField } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import {addToCart,incrementQuantity,decrementQuantity,removeItem} from '../redux/cartSlice';
 import { useSelector } from 'react-redux';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
+import { alignProperty } from '@mui/material/styles/cssUtils';
 
 export default function ShoppingCard(props) {
   const {id,img,name,price} = props.details;
   const dispatch = useDispatch();
   const cart = useSelector((state) => state.cart)
   const displayButton=cart.find((item) => item.id === id)
+  // const quantity = cart.find((item) => {if(item.id === id){console.log(item)
+  //                           return item.quantity}})
+  const [quantity,setQuantity] = React.useState('0');
 
+  const retreiveQty = () => {
+    cart.find((item) => {
+      if (item.id === id) {
+        setQuantity(item.quantity);
+      }
+    })
+  }
+  React.useEffect(()=>{
+    retreiveQty();
+  },[cart])
   console.log("shopping card: ",props.details);
   return (
     <Grid item xs={12} sm={4} md={4} > 
@@ -45,7 +59,7 @@ export default function ShoppingCard(props) {
         <Button size="small" color="success" onClick={() => 
     {dispatch(addToCart({
       id, name, img, price
-    }))}
+    }));}
   }>Add to Cart</Button>
         {displayButton && <Button
             size="small"
@@ -54,11 +68,17 @@ export default function ShoppingCard(props) {
             color="success"
             onClick={() => 
               {dispatch(incrementQuantity(
-                id))}
+                id));}
             }
           >
             +
           </Button>}
+          {displayButton && <TextField sx={{
+        width: { sm: 80, md: 80,ml:10,p:2 },
+        "& .MuiInputBase-root": {
+            height: 30
+        }
+    }} size="small" id="quantity-field" value={quantity} variant="filled" disabled/>}
           {displayButton && <Button
             size="small"
             disableElevation
@@ -66,7 +86,7 @@ export default function ShoppingCard(props) {
             color="success"
             onClick={() => 
               {dispatch(decrementQuantity(
-                id))}
+                id));}
             }
           >
             -
